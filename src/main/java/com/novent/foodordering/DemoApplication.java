568d648f9a0d48a83ce0e41d521e684f20597f0d
Raghad Almanasrah <rmanasrah@noventapp.com>
2017-11-13 10:23:28 +0200

    update demoApp

--- src/main/java/com/novent/foodordering/DemoApplication.java
+++ src/main/java/com/novent/foodordering/DemoApplication.java
@@ -2,7 +2,9 @@ package com.novent.foodordering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.novent")
 @SpringBootApplication
 public class DemoApplication {
 
