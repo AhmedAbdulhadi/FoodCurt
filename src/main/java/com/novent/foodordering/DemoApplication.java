3235232f3e36ff1b553f68973da7631225de870d
Raghad Almanasrah <rmanasrah@noventapp.com>
2018-01-17 14:48:42 +0200

    add variable

--- src/main/java/com/novent/foodordering/DemoApplication.java
+++ src/main/java/com/novent/foodordering/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.novent.foodordering;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,13 +15,16 @@ public class DemoApplication extends SpringBootServletInitializer{
 //	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplicationTests.class, args);
 //	}
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
 	// for JAR
 		public static void main(String[] args) {
 			System.out.println("Demo app appli.prop");
 			System.out.println(System.getenv("database"));
+			LOGGER.info(System.getenv("database"));
 			System.out.println(System.getenv("username"));
+			LOGGER.info(System.getenv("username"));
 			System.out.println(System.getenv("password"));
+			LOGGER.info(System.getenv("password"));
 
 			SpringApplication.run(DemoApplication.class, args);
 		}
