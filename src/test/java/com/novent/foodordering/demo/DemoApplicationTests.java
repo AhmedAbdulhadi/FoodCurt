18196be5e624494e5d61e7f66fa1f230dfa4fb82
Raghad Almanasrah <rmanasrah@noventapp.com>
2018-01-18 10:58:02 +0200

    add logs

--- src/test/java/com/novent/foodordering/demo/DemoApplicationTests.java
+++ src/test/java/com/novent/foodordering/demo/DemoApplicationTests.java
@@ -2,14 +2,35 @@ package com.novent.foodordering.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.novent.foodordering.DemoApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+//	System.out.println("Demo app appli.prop");
+//	LOGGER.info("Demo app appli.prop");
+//	System.out.println(System.getenv("database"));
+//	LOGGER.info(System.getenv("database"));
+//	System.out.println(System.getenv("username"));
+//	LOGGER.info(System.getenv("username"));
+//	System.out.println(System.getenv("password"));
+//	LOGGER.info(System.getenv("password"));
 	
 	@Test
 	public void contextLoads() {
+		System.out.println("Demo app appli.prop");
+		LOGGER.info("Demo app appli.prop");
+		System.out.println(System.getenv("database"));
+		LOGGER.info(System.getenv("database"));
+		System.out.println(System.getenv("username"));
+		LOGGER.info(System.getenv("username"));
+		System.out.println(System.getenv("password"));
+		LOGGER.info(System.getenv("password"));
 	}
 	}
